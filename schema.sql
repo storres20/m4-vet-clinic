28ea6801fd8cbf0e56db1af15d19fa9675ebe5ef
Italo Lon kan <sanchotorres777@gmail.com>
2023-01-17 17:21:42 -0500

    Add new table named animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int,
+    name text,
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
 );
